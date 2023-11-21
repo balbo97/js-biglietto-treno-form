85ffa7e6a1f363f7c724adc763c146becd24d237
balbo97 <balbosauro1@gmail.com>
2023-11-21 20:18:02 +0100

    MILESTONE 2 completata

--- js/script.js
+++ js/script.js
@@ -20,17 +20,28 @@ button.addEventListener('click', function(){
     let costo_biglietto;
     if (anni == 1){
         //UNDER 18
-        costo_biglietto = costo_biglietto_km - costo_biglietto_km * 0.2
+        costo_biglietto = costo_biglietto_km - costo_biglietto_km * 0.2;
+        document.getElementById('welcome').innerText = `Welcome ${name} ${surname}!`;
+        document.getElementById('discount').innerText = `Rientri nella fascia under 18, hai diritto ad uno sconto del 20% sul costo iniziale di ${costo_biglietto_km.toFixed(2)}\u20AC.`;
+        
     }
     else if(anni == 3){
         //OVER 65
-        costo_biglietto = costo_biglietto_km - costo_biglietto_km * 0.4
+        costo_biglietto = costo_biglietto_km - costo_biglietto_km * 0.4;
+        document.getElementById('welcome').innerText = `Welcome ${name} ${surname}!`;
+        document.getElementById('discount').innerText = `Rientri nella fascia over 65, hai diritto ad uno sconto del 40% sul costo iniziale di ${costo_biglietto_km.toFixed(2)}\u20AC.`;
+        
     }
     else{
         costo_biglietto = costo_biglietto_km
+        document.getElementById('welcome').innerText = `Welcome ${name} ${surname}!`;
+        document.getElementById('discount').innerText = ''
     }
 
     //COSTO DEL BIGLIETTO MOSTRATO IN CONSOLE.LOG
-    console.log(costo_biglietto.toFixed(2));
+    console.log(costo_biglietto);
+
+    //PREZZO BIGLIETTO MOSTRATO SUL DISPLAY
+    document.getElementById('ticket').innerText =`Prezzo biglietto: ${costo_biglietto.toFixed(2)} \u20AC`
 
 })
